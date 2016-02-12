885e9d1ab424ef3a994e678bd0fffa2d4ec6e19e
Jani Piippo <jani.piippo@student.tut.fi>
2016-02-12 15:12:50 +0200

    Plain text totetutettu. Emailin toteuttaminen jäljellä.

--- InvoiceGenerator/main.py
+++ InvoiceGenerator/main.py
@@ -29,14 +29,14 @@ def calculate_ref_nro(invoice_type, year, member_number):
 
 def create_payment_options_str(invoice_info, member_number, current_year, plain_text=False):
     payment_options_str = ""
-    endline = "<br>"
+    newline = "<br>"
     if plain_text:
-        endline = "\n"
+        newline = "\n"
     for row in invoice_info:
-        newline = "Invoice type: " + row['Type'] + \
+        addline = "Invoice type: " + row['Type'] + \
                   ", Total: " + row['Total'] + ", Reference Number: " + \
-                  calculate_ref_nro(row['ID'], current_year, member_number) + endline
-        payment_options_str += newline
+                  calculate_ref_nro(row['ID'], current_year, member_number) + newline
+        payment_options_str += addline
     return payment_options_str
 
 
@@ -45,50 +45,80 @@ def main():
     invoice_path = './invoice_types.csv'
     member_path = './member_list.csv'
     html_template_path = './templates/html_template.txt'
+    text_template_path = './templates/plain_text_template.txt'
     current_year_long = strftime('%Y', localtime())
     current_year_short = strftime('%y', localtime())
-
+    html_template = ''
+    text_template = ''
+    my_email = 'info@tampereenurheiluampujat.fi'
 
     # Import invoice HTML template
     with open(html_template_path, 'r') as template_file:
         html_template = template_file.read().replace('\n', '')
-        print(html_template)
 
-        # Define invoices
-        # Invoice_info has two fields per row: Invoice type, and invoice total
-        with open(invoice_path, 'r') as inv_file:
-            invoice_info = list(csv.DictReader(inv_file, delimiter=';'))
+    # Import invoice plain text template
+    with open(text_template_path, 'r') as template_file:
+        text_template = template_file.read().replace('\n', '')
 
-            # Read recipient information from CSV file
-            with open(member_path, 'r') as mem_file:
-                members = csv.DictReader(mem_file, delimiter=';')
+    # Define invoices
+    # Invoice_info has two fields per row: Invoice type, and invoice total
+    with open(invoice_path, 'r') as inv_file:
+        invoice_info = list(csv.DictReader(inv_file, delimiter=';'))
 
-                # Go through each recipient, calculate reference number,
-                # create text for email and send it
-                for row in members:
-                    member_number = row['membernumber']
-                    member_type = row['membertype']
-                    first_name = row['firstname']
-                    surname = row['surname']
-                    address = row['address']
-                    zip = row['zip']
-                    city = row['city']
-                    recipient_email = row['email']
-                    payment_options = create_payment_options_str(invoice_info, member_number, current_year_short)
-                    print(payment_options)
-                    html_msg = html_template.format(firstname=first_name,
-                                            surname=surname,
-                                            address=address,
-                                            zip=zip,
-                                            city=city,
-                                            recipientemail=recipient_email,
-                                            membernumber=member_number,
-                                            membertype=member_type,
-                                            paymentoptions=payment_options,
-                                            year=current_year_long)
-                    print(html_msg)
-                    with open("./emails/"+member_number+surname+first_name+current_year_long+".html", 'w') as html_file:
-                        html_file.write(html_msg)
+        # Read recipient information from CSV file
+        with open(member_path, 'r') as mem_file:
+            members = csv.DictReader(mem_file, delimiter=';')
+
+            # Go through each recipient, calculate reference number,
+            # create text for email and send it
+            for row in members:
+                member_number = row['membernumber']
+                member_type = row['membertype']
+                first_name = row['firstname']
+                surname = row['surname']
+                address = row['address']
+                zip = row['zip']
+                city = row['city']
+                recipient_email = row['email']
+                html_msg = html_template.format(firstname=first_name,
+                                        surname=surname,
+                                        address=address,
+                                        zip=zip,
+                                        city=city,
+                                        recipientemail=recipient_email,
+                                        membernumber=member_number,
+                                        membertype=member_type,
+                                        paymentoptions=create_payment_options_str(invoice_info,
+                                                                                  member_number,
+                                                                                  current_year_short),
+                                        year=current_year_long)
+                text_msg = text_template.format(firstname=first_name,
+                                        surname=surname,
+                                        address=address,
+                                        zip=zip,
+                                        city=city,
+                                        recipientemail=recipient_email,
+                                        membernumber=member_number,
+                                        membertype=member_type,
+                                        paymentoptions=create_payment_options_str(invoice_info,
+                                                                                  member_number,
+                                                                                  current_year_short, True),
+                                        year=current_year_long)
+                print(html_msg)
+                print(text_msg)
+                with open("./emails/"+member_number+surname+first_name+current_year_long+".html", 'w') as html_file:
+                    html_file.write(html_msg)
+
+                with open("./emails/"+member_number+surname+first_name+current_year_long+".txt", 'w') as txt_file:
+                    txt_file.write(text_msg)
+
+                msg = MIMEMultipart('alternateive')
+                msg['Subject'] = 'Test invoice from TaU'
+                msg['From'] = my_email
+                msg['To'] = recipient_email
+                msg.attach(MIMEText(text_msg, 'plain'))
+                msg.attach(MIMEText(html_msg, 'html'))
+                
 
 
 
